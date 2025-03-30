bf0c96170c78a5739c1c8414af15d1eb7755ba44
Uelinton <uelintonpaulo2@gmail.com>
2025-03-30 19:31:38 -0400

    created mediator entity for enimies

--- Code/Level.py
+++ Code/Level.py
@@ -8,6 +8,7 @@ from pygame.font import Font
 from Code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from Code.Entity import Entity
 from Code.EntityFactory import EntityFactory
+from Code.EntityMediator import EntityMediator
 
 
 class Level:
@@ -45,6 +46,8 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
