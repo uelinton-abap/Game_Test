ea3832db1ef5f19173324b2959c177546d7fd3d5
Uelinton <uelintonpaulo2@gmail.com>
2025-03-30 13:04:28 -0400

    Adding classes

---
+++ Code/Level.py
@@ -0,0 +1,13 @@
+from enum import nonmember
+
+
+class Level:
+
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
---
+++ Code/Player.py
@@ -0,0 +1,12 @@
+from Entity import Entity
+
+
+class Player(Entity):
+    def __init__(self):
+        pass
+
+    def update(self, ):
+        pass
+
+    def move(self, ):
+        pass
